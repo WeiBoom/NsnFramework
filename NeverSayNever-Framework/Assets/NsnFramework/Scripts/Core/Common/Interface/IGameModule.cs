e45273e36042b694a59c09623c25e1bd969b1222
Boom <631450364@qq.com>
2021-04-09 15:00:28 +0800

    添加‘模块’接口

---
+++ NeverSayNever-Framework/Assets/NsnFramework/Scripts/Core/Common/Interface/IGameModule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace NeverSayNever.Core
+{
+    public interface IGameModule
+    {
+        void OnInitialize();
+        void OnUpdate(float deltaTime);
+        void OnDispose();
+    }
+}
---
+++ NeverSayNever-Framework/Assets/NsnFramework/Scripts/Core/Common/UGameModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NeverSayNever.Core
+{
+    public class GameModule : IGameModule
+    {
+
+        // 模块初始化
+        public virtual void OnInitialize()
+        {
+            RegisterEvents();
+        }
+
+        // 模块更新
+        public virtual void OnUpdate(float deltaTime)
+        {
+        }
+
+        // 模块释放（销毁）
+        public virtual void OnDispose()
+        {
+        }
+
+        // 模块注册事件
+        protected virtual void RegisterEvents()
+        {
+        }
+    }
+}

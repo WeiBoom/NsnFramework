9f182dcb132b2adf27677b54a2f97f7a38ac153c
Boom <631450364@qq.com>
2023-02-10 02:05:22 +0800

    Add Interface

---
+++ NeverSayNever-Framework/Assets/NsnFramework/Scripts/Interface/IAssetMgr.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Nsn
+{
+    using YooAsset;
+
+    public interface IAssetMgr
+    {
+        int GetResourceVersion();
+
+        AssetOperationHandle LoadAsset(Type type, string location);
+
+        AssetOperationHandle LoadAsset<T>(string location) where T : UnityEngine.Object;
+
+        AssetOperationHandle LoadAssetAsync<T>(string location) where T : UnityEngine.Object;
+
+        void UnloadUnusedAssets();
+        void ForceUnloadAllAssets();
+
+    }
+}
--- NeverSayNever-Framework/Assets/NsnFramework/Scripts/Modules/Mdl.Res/ResMgr.cs
+++ NeverSayNever-Framework/Assets/NsnFramework/Scripts/Modules/Mdl.Res/ResMgr.cs
@@ -1,46 +1,77 @@
-
 using System;
 
 namespace Nsn
 {
-    public class ResMgr : IResMgr
+    using System.Collections.Generic;
+    using YooAsset;
+
+    public class ResMgr : IAssetMgr
     {
+
+        private YooAssets.InitializeParameters _initParameters;
+
         public void OnInitialized(params object[] args)
         {
+            _initParameters = args[0] as YooAssets.InitializeParameters;
+            if(_initParameters == null)
+                throw new Exception("ResMgr initialize failed , params is invalid .");
+
+            InitializationOperation operation = InitializeAsync();
+        }
+
+        private InitializationOperation InitializeAsync()
+        {
+			return YooAssets.InitializeAsync(_initParameters);
         }
 
         public void OnDisposed()
         {
+            ForceUnloadAllAssets();
         }
 
         public void OnUpdate(float deltaTime)
         {
         }
 
-
         public int GetResourceVersion()
         {
-            return 0;
+            return YooAssets.GetResourceVersion();
         }
 
-        public void LoadAsset(Type type, string location)
+        public AssetOperationHandle LoadAsset(Type type, string location)
         {
+            return YooAssets.LoadAssetSync(location, type);
         }
 
-        public void LoadAsset<T>(string location) where T : UnityEngine.Object
+        public AssetOperationHandle LoadAsset<T>(string location) where T : UnityEngine.Object
         {
+            return YooAssets.LoadAssetSync<T>(location);
         }
 
-        public void LoadAssetAsync(Type type, string location)
+        public AssetOperationHandle LoadAssetAsync(Type type, string location)
         {
+            return YooAssets.LoadAssetAsync(location, type);
         }
 
-        public void LoadAssetAsync<T>(string location) where T : UnityEngine.Object
+        public AssetOperationHandle LoadAssetAsync<T>(string location) where T : UnityEngine.Object
         {
+            return YooAssets.LoadAssetAsync<T>(location);
         }
 
         public void UnloadUnusedAssets()
         {
+            YooAssets.UnloadUnusedAssets();
         }
+
+        public void ForceUnloadAllAssets()
+        {
+            YooAssets.ForceUnloadAllAssets();
+        }
+
+        public void Release(AssetOperationHandle handle)
+        {
+            handle.Release();
+        }
+
     }
 }
